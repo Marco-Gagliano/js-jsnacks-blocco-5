86360855160d6c9c4bfd838c7755825ab9dd7901
Marco-Gagliano <brazil_94@hotmail.it>
2022-04-28 17:24:40 +0200

    creazione-array-object

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -3,3 +3,65 @@
   Crea 10 oggetti che rappresentano una zucchina.
   Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine.
 */
+
+const zucchine = [
+  {
+  varietàZucchina: "Verde",
+  pesoZucchina: "50",
+  lunghezzaZucchina: "17",
+  },
+  
+  {
+  varietàZucchina: "Tonda",
+  pesoZucchina: "60",
+  lunghezzaZucchina: "16",
+  },
+  
+  {
+  varietàZucchina: "Lunga",
+  pesoZucchina: "80",
+  lunghezzaZucchina: "19",
+  },
+  
+  {
+  varietàZucchina: "Gialla",
+  pesoZucchina: "90",
+  lunghezzaZucchina: "18",
+  },
+  
+  {
+  varietàZucchina: "Striata",
+  pesoZucchina: "110",
+  lunghezzaZucchina: "17",
+  },
+  
+  {
+  varietàZucchina: "Romanesca",
+  pesoZucchina: "70",
+  lunghezzaZucchina: "16",
+  },
+  
+  {
+  varietàZucchina: "Nera",
+  pesoZucchina: "100",
+  lunghezzaZucchina: "17",
+  },
+  
+  {
+  varietàZucchina: "Bianca",
+  pesoZucchina: "120",
+  lunghezzaZucchina: "19",
+  },
+  
+  {
+  varietàZucchina: "Trombetta",
+  pesoZucchina: "130",
+  lunghezzaZucchina: "20",
+  },
+  
+  {
+  varietàZucchina: "Alberello",
+  pesoZucchina: "150",
+  lunghezzaZucchina: "25",
+  },
+]
