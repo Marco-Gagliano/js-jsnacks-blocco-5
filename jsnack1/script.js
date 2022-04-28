de4d7900a997272cf245ee10e8f5ebe0d6c4e1a4
Marco-Gagliano <brazil_94@hotmail.it>
2022-04-28 15:11:09 +0200

    creazione-array-object

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,3 +3,66 @@
   Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
   Calcola quanto pesano tutte le zucchine.
 */
+
+const zucchine = [
+  {
+  varietàZucchina: "Verde",
+  pesoZucchina: "50",
+  lunghezzaZucchina: "17",
+  },
+  
+  {
+  varietàZucchina: "Tonda",
+  pesoZucchina: "60",
+  lunghezzaZucchina: "16",
+  },
+  
+  {
+  varietàZucchina: "Lunga",
+  pesoZucchina: "80",
+  lunghezzaZucchina: "19",
+  },
+  
+  {
+  varietàZucchina: "Gialla",
+  pesoZucchina: "90",
+  lunghezzaZucchina: "18",
+  },
+  
+  {
+  varietàZucchina: "Striata",
+  pesoZucchina: "110",
+  lunghezzaZucchina: "17",
+  },
+  
+  {
+  varietàZucchina: "Romanesca",
+  pesoZucchina: "70",
+  lunghezzaZucchina: "16",
+  },
+  
+  {
+  varietàZucchina: "Nera",
+  pesoZucchina: "100",
+  lunghezzaZucchina: "17",
+  },
+  
+  {
+  varietàZucchina: "Bianca",
+  pesoZucchina: "120",
+  lunghezzaZucchina: "19",
+  },
+  
+  {
+  varietàZucchina: "Trombetta",
+  pesoZucchina: "130",
+  lunghezzaZucchina: "20",
+  },
+  
+  {
+  varietàZucchina: "Alberello",
+  pesoZucchina: "150",
+  lunghezzaZucchina: "25",
+  },
+]
+
